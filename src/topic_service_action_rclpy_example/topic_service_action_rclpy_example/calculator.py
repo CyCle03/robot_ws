004253eb2c1d4ab0145c39062d3a3c2e15ec3a8e
CyCle03 <cherrybear03@naver.com>
2026-02-10 14:39:29 +0900

    Fix subscriber filename and add launch assets

--- src/topic_service_action_rclpy_example/topic_service_action_rclpy_example/calculator.py
+++ src/topic_service_action_rclpy_example/topic_service_action_rclpy_example/calculator.py
@@ -1,6 +1,9 @@
 import time
 from msg_srv_action_interface_example.msg import ArithmeticArgument
 from msg_srv_action_interface_example.srv import ArithmeticOperator
+from msg_srv_action_interface_example.action import ArithmeticChecker
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.action import ActionServer
 from rclpy.node import Node
 from rclpy.qos import QoSDurabilityPolicy
 from rclpy.qos import QoSHistoryPolicy
@@ -8,9 +11,6 @@ from rclpy.qos import QoSProfile
 from rclpy.qos import QoSReliabilityPolicy
 from rclpy.callback_groups import ReentrantCallbackGroup
 import rclpy
-from msg_srv_action_interface_example.action import ArithmeticChecker
-from rclpy.action import ActionServer
-from rclpy.executors import MultiThreadedExecutor
 
 class Calculator(Node):
 
@@ -53,47 +53,6 @@ class Calculator(Node):
             callback_group=self.callback_group)
 
 
-    def get_arithmetic_operator(self, request, response):
-        self.argument_operator = request.arithmetic_operator
-
-        self.argument_result = self.calculate_given_formula(
-            self.argument_a,
-            self.argument_b,
-            self.argument_operator)
-
-        response.arithmetic_result = self.argument_result
-
-        self.argument_formula = '{0} {1} {2} = {3}'.format(
-                self.argument_a,
-                self.operator[self.argument_operator-1],
-                self.argument_b,
-                self.argument_result)
-
-        self.get_logger().info(self.argument_formula)
-
-        return response
-
-    def calculate_given_formula(self, a, b, operator):
-        if operator == ArithmeticOperator.Request.PLUS:
-            self.argument_result = a + b
-        elif operator == ArithmeticOperator.Request.MINUS:
-            self.argument_result = a - b
-        elif operator == ArithmeticOperator.Request.MULTIPLY:
-            self.argument_result = a * b
-        elif operator == ArithmeticOperator.Request.DIVISION:
-            try:
-                self.argument_result = a / b
-            except ZeroDivisionError:
-                self.get_logger().error('ZeroDivisionError!')
-                self.argument_result = 0.0
-                return self.argument_result
-        else:
-            self.get_logger().error(
-                'Please make sure arithmetic operator(plus, minus, multiply, division).')
-            self.argument_result = 0.0
-        return self.argument_result
-
-
     def get_arithmetic_argument(self, msg):
       self.argument_a = msg.argument_a
       self.argument_b = msg.argument_b
@@ -101,28 +60,63 @@ class Calculator(Node):
       self.get_logger().info('Subscribed argument a: {0}'.format(self.argument_a))
       self.get_logger().info('Subscribed argument b: {0}'.format(self.argument_b))
 
+    def get_arithmetic_operator(self, request, response):
+      self.argument_operator = request.arithmetic_operator
+      self.argument_result = self.calculate_given_formula(
+          self.argument_a,
+          self.argument_b,
+          self.argument_operator)
+      response.arithmetic_result = self.argument_result
+      self.argument_formula = '{0} {1} {2} = {3}'.format(
+              self.argument_a,
+              self.operator[self.argument_operator-1],
+              self.argument_b,
+              self.argument_result)
+      self.get_logger().info(self.argument_formula)
+      return response
+
+    def calculate_given_formula(self, a, b, operator):
+      if operator == ArithmeticOperator.Request.PLUS:
+          self.argument_result = a + b
+      elif operator == ArithmeticOperator.Request.MINUS:
+          self.argument_result = a - b
+      elif operator == ArithmeticOperator.Request.MULTIPLY:
+          self.argument_result = a * b
+      elif operator == ArithmeticOperator.Request.DIVISION:
+          try:
+              self.argument_result = a / b
+          except ZeroDivisionError:
+              self.get_logger().error('ZeroDivisionError!')
+              self.argument_result = 0.0
+              return self.argument_result
+      else:
+          self.get_logger().error(
+              'Please make sure arithmetic operator(plus, minus, multiply, division).')
+          self.argument_result = 0.0
+      return self.argument_result
+
     def execute_checker(self, goal_handle):
-        self.get_logger().info('Execute arithmetic_checker action!')
+      self.get_logger().info('Execute arithmetic_checker action!')
 
-        feedback_msg = ArithmeticChecker.Feedback()
-        feedback_msg.formula = []
-        total_sum = 0.0
-        goal_sum = goal_handle.request.goal_sum
+      feedback_msg = ArithmeticChecker.Feedback()
+      feedback_msg.formula = []
+      total_sum = 0.0
+      goal_sum = goal_handle.request.goal_sum
 
-        while total_sum < goal_sum:
-            total_sum += self.argument_result
-            feedback_msg.formula.append(self.argument_formula)
-            self.get_logger().info('Feedback: {0}'.format(feedback_msg.formula))
-            goal_handle.publish_feedback(feedback_msg)
-            time.sleep(1)
+      while total_sum < goal_sum:
+          total_sum += self.argument_result
+          feedback_msg.formula.append(self.argument_formula)
+          self.get_logger().info('Feedback: {0}'.format(feedback_msg.formula))
+          goal_handle.publish_feedback(feedback_msg)
+          time.sleep(1)
 
-        goal_handle.succeed()
+      goal_handle.succeed()
 
-        result = ArithmeticChecker.Result()
-        result.all_formula = feedback_msg.formula
-        result.total_sum = total_sum
+      result = ArithmeticChecker.Result()
+      result.all_formula = feedback_msg.formula
+      result.total_sum = total_sum
 
-        return result
+      return result
 
 
 def main(args=None):
@@ -145,4 +139,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
